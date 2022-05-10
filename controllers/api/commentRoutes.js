15eadb4db94550f61b3819689f3dfbbd5d85b7fa
coderbennett <jbennett@csumb.edu>
2022-05-10 11:40:11 -0700

    add post route for commenting on a post

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -23,3 +23,19 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            body: req.body.body,
+            reader_id: req.session.user_id,
+            post_id: req.body.post_id
+        });
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
