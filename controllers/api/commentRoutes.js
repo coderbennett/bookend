00d206824f42e5eb457ca65d947efdeeae5fb396
coderbennett <jbennett@csumb.edu>
2022-05-10 11:44:58 -0700

    add put route for comments to be update

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -38,4 +38,23 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id.'});
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 module.exports = router;
