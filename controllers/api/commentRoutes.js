18da2a6226203eeb030c10932edd650ad29b33b0
coderbennett <jbennett@csumb.edu>
2022-05-10 11:47:12 -0700

    add delete route for comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -57,4 +57,24 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentData = await.Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+
+        if (!commentData) {
+            res.status(404).json({message: 'No comment was found with this id.'});
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
