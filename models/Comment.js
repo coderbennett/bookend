523644619fd795d8dfdb9a001b456a8ce11a7d57
atmason90 <atmason90@gmail.com>
2022-05-10 10:40:13 -0700

    finish Comment model

---
+++ models/Comment.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        reader_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'reader',
+                key: 'id',
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+    },
+);
+
+module.exports = Comment;
