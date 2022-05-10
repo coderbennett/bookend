b608b98457ce7431469d02b18731f2929335fede
coderbennett <jbennett@csumb.edu>
2022-05-10 15:02:38 -0700

    add changes to allow data to be seeded

--- models/Club.js
+++ models/Club.js
@@ -9,19 +9,22 @@ Club.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             unique: true,
+            primaryKey: true,
             autoIncrement: true,
         },
         name: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
-        },
+        }
+    },
+    {
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'club'
-    },
+    }
 );
 
 module.exports = Club;
