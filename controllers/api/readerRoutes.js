971312e785465e51fdfcabccad087b60d88da74c
coderbennett <jbennett@csumb.edu>
2022-05-10 10:38:46 -0700

    add reader get route for reader with specific id

--- controllers/api/readerRoutes.js
+++ controllers/api/readerRoutes.js
@@ -10,3 +10,13 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const readerData = await Reader.findByPk(req.params.id);
+
+        res.status(200).json(readerData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
