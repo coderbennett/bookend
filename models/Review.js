4412d59e201dc588f030af7d0e53ebe50242fd77
atmason90 <atmason90@gmail.com>
2022-05-10 10:57:08 -0700

    finish Review model

---
+++ models/Review.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {};
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        reader_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'reader',
+                key: 'id',
+            },
+        },
+        book_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'book',
+                key: 'id',
+            },
+        },
+    },
+);
+
+module.exports = Review;
