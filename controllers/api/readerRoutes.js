86ed0b02dcc08443dcba3138a99aa9b7c1138f53
coderbennett <jbennett@csumb.edu>
2022-05-10 10:42:14 -0700

    add post route for creating a reader account

--- controllers/api/readerRoutes.js
+++ controllers/api/readerRoutes.js
@@ -20,3 +20,22 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const readerData = await Reader.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.user_id = readerData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(readerData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
