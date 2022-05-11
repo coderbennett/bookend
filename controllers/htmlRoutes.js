d905c801828ae8344d25ac3b53fffbdea10d4d67
John Netzel <johnrnetzel@gmail.com>
2022-05-11 09:05:20 -0800

    Add book title to book page

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
+const { Book } = require('../models');
 
 router.get("/", (req, res) =>
 {
     res.render("homepage", { logged_in: req.session.LoggedIn });
 });
 
-router.get("/book/:id", (req, res) =>
+router.get("/book/:id", async (req, res) =>
 {
-    res.render("book", { logged_in: req.session.LoggedIn });
+    const bookData = await Book.findByPk(req.params.id/*, {
+        include: [{ model: Book }]
+    }*/);
+
+    if (!bookData) {
+        return res.status(404).json({message: 'No book found with this id.'});
+    }
+
+    const book = bookData.get();
+
+    res.render("book", { book: book, logged_in: req.session.LoggedIn });
 });
 
 module.exports = router;
