0cca4294f6ab7dd19ce190b189312b7f575037b8
atmason90 <atmason90@gmail.com>
2022-05-11 11:23:35 -0700

    add post and delete club routes

--- controllers/api/clubRoutes.js
+++ controllers/api/clubRoutes.js
@@ -29,4 +29,28 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    // console.log(req.body);
+    try {
+        const newClub = await Club.create({ ...req.body, reader_id: req.session.reader_id });
+        // console.log(newClub);
+        res.json(newClub);
+    } catch (err) {
+        // console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const delClub = Club.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
