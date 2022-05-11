cbcf52647e3def486d47da61441f411c28876fc8
atmason90 <atmason90@gmail.com>
2022-05-10 18:23:50 -0700

    write clubPageRoutes

--- controllers/clubPageRoutes.js
+++ controllers/clubPageRoutes.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
 const { Club, Book } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/')
+router.get('/', async (req, res) => {
+    try {
+        const allClubs = await Club.findAll();
+        res.render('homepage', {
+            layout: 'main',
+            allClubs,
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const clubData = await Club.findOne({
+            where: {id: req.params.id}
+        });
+        res.render('clubpage', {
+            layout: 'main',
+            clubData
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
