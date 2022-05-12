3f80f25ee7914349627a0ea9e123ec29968d7523
atmason90 <atmason90@gmail.com>
2022-05-12 11:42:31 -0700

    add with auth to clubpage routes

--- controllers/clubPageRoutes.js
+++ controllers/clubPageRoutes.js
@@ -14,11 +14,11 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/new', async (req, res) => {
+router.get('/new', withAuth, async (req, res) => {
         res.render('newclub', { logged_in: req.session.LoggedIn});
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', withAuth, async (req, res) => {
     try {
         const clubData = await Club.findByPk(req.params.id, {
             include: Book
